dc0dfec20065e0ccba08c35971f8d9884c531efb
ViXiV Technologies <ViXiVTech@gmail.com>
2018-11-04 21:03:48 -0500

    Update main.js

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function($){
-    $('.superpwa-colorpicker').wpColorPicker();	// Color picker
-	$('.superpwa-icon-upload').click(function(e) {	// Application Icon upload
+    $('.pwapro-colorpicker').wpColorPicker();	// Color picker
+	$('.pwapro-icon-upload').click(function(e) {	// Application Icon upload
 		e.preventDefault();
-		var superpwa_meda_uploader = wp.media({
+		var pwapro_meda_uploader = wp.media({
 			title: 'Application Icon',
 			button: {
 				text: 'Select Icon'
@@ -10,14 +10,14 @@ jQuery(document).ready(function($){
 			multiple: false  // Set this to true to allow multiple files to be selected
 		})
 		.on('select', function() {
-			var attachment = superpwa_meda_uploader.state().get('selection').first().toJSON();
-			$('.superpwa-icon').val(attachment.url);
+			var attachment = pwapro_meda_uploader.state().get('selection').first().toJSON();
+			$('.pwapro-icon').val(attachment.url);
 		})
 		.open();
 	});
-	$('.superpwa-splash-icon-upload').click(function(e) {	// Splash Screen Icon upload
+	$('.pwapro-splash-icon-upload').click(function(e) {	// Splash Screen Icon upload
 		e.preventDefault();
-		var superpwa_meda_uploader = wp.media({
+		var pwapro_meda_uploader = wp.media({
 			title: 'Splash Screen Icon',
 			button: {
 				text: 'Select Icon'
@@ -25,8 +25,8 @@ jQuery(document).ready(function($){
 			multiple: false  // Set this to true to allow multiple files to be selected
 		})
 		.on('select', function() {
-			var attachment = superpwa_meda_uploader.state().get('selection').first().toJSON();
-			$('.superpwa-splash-icon').val(attachment.url);
+			var attachment = pwapro_meda_uploader.state().get('selection').first().toJSON();
+			$('.pwapro-splash-icon').val(attachment.url);
 		})
 		.open();
 	});

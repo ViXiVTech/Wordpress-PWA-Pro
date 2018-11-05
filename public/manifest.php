9b3675a31f127f2cceef05192cb9a06ccb5ec8a3
ViXiV Technologies <ViXiVTech@gmail.com>
2018-11-04 20:49:16 -0500

    Update manifest.php

--- public/manifest.php
+++ public/manifest.php
@@ -4,14 +4,14 @@
  *
  * @since 1.0
  * 
- * @function	superpwa_manifest()						Manifest filename, absolute path and link
- * @function	superpwa_generate_manifest()			Generate and write manifest
- * @function	superpwa_add_manifest_to_wp_head()		Add manifest to header (wp_head)
- * @function	superpwa_register_service_worker()		Register service worker in the footer (wp_footer)
- * @function	superpwa_delete_manifest()				Delete manifest
- * @function 	superpwa_get_pwa_icons()				Get PWA Icons
- * @function	superpwa_get_scope()					Get navigation scope of PWA
- * @function	superpwa_get_orientation()				Get orientation of PWA
+ * @function	pwapro_manifest()						Manifest filename, absolute path and link
+ * @function	pwapro_generate_manifest()			Generate and write manifest
+ * @function	pwapro_add_manifest_to_wp_head()		Add manifest to header (wp_head)
+ * @function	pwapro_register_service_worker()		Register service worker in the footer (wp_footer)
+ * @function	pwapro_delete_manifest()				Delete manifest
+ * @function 	pwapro_get_pwa_icons()				Get PWA Icons
+ * @function	pwapro_get_scope()					Get navigation scope of PWA
+ * @function	pwapro_get_orientation()				Get orientation of PWA
  */
 
 // Exit if accessed directly
@@ -20,20 +20,20 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**
  * Manifest filename, absolute path and link
  *
- * For Multisite compatibility. Used to be constants defined in superpwa.php
+ * For Multisite compatibility. Used to be constants defined in pwapro.php
  * On a multisite, each sub-site needs a different manifest file.
  *
- * @param $arg 	filename for manifest filename (replaces SUPERPWA_MANIFEST_FILENAME)
- *				abs for absolute path to manifest (replaces SUPERPWA_MANIFEST_ABS)
- *				src for link to manifest (replaces SUPERPWA_MANIFEST_SRC). Default value
+ * @param $arg 	filename for manifest filename (replaces PWAPRO_MANIFEST_FILENAME)
+ *				abs for absolute path to manifest (replaces PWAPRO_MANIFEST_ABS)
+ *				src for link to manifest (replaces PWAPRO_MANIFEST_SRC). Default value
  *
  * @return String filename, absolute path or link to manifest. 
  *  
  * @since 1.6
  */
-function superpwa_manifest( $arg = 'src' ) {
+function pwapro_manifest( $arg = 'src' ) {
 	
-	$manifest_filename = 'superpwa-manifest' . superpwa_multisite_filename_postfix() . '.json';
+	$manifest_filename = 'pwapro-manifest' . pwapro_multisite_filename_postfix() . '.json';
 	
 	switch( $arg ) {
 		
@@ -65,12 +65,12 @@ function superpwa_manifest( $arg = 'src' ) {
  * @since 1.4 Added orientation and scope.
  * @since 1.5 Added gcm_sender_id
  * @since 1.6 Added description
- * @since 1.8 Removed gcm_sender_id and introduced filter superpwa_manifest. gcm_sender_id is added in /3rd-party/onesignal.php
+ * @since 1.8 Removed gcm_sender_id and introduced filter pwapro_manifest. gcm_sender_id is added in /3rd-party/onesignal.php
  */
-function superpwa_generate_manifest() {
+function pwapro_generate_manifest() {
 	
 	// Get Settings
-	$settings = superpwa_get_settings();
+	$settings = pwapro_get_settings();
 	
 	$manifest 						= array();
 	$manifest['name']				= $settings['app_name'];
@@ -81,22 +81,22 @@ function superpwa_generate_manifest() {
 		$manifest['description'] 	= $settings['description'];
 	}
 	
-	$manifest['icons']				= superpwa_get_pwa_icons();
+	$manifest['icons']				= pwapro_get_pwa_icons();
 	$manifest['background_color']	= $settings['background_color'];
 	$manifest['theme_color']		= $settings['theme_color'];
 	$manifest['display']			= 'standalone';
-	$manifest['orientation']		= superpwa_get_orientation();
-	$manifest['start_url']			= superpwa_get_start_url( true );
-	$manifest['scope']				= superpwa_get_scope();
+	$manifest['orientation']		= pwapro_get_orientation();
+	$manifest['start_url']			= pwapro_get_start_url( true );
+	$manifest['scope']				= pwapro_get_scope();
 	
 	// Filter the manifest.
-	$manifest = apply_filters( 'superpwa_manifest', $manifest );
+	$manifest = apply_filters( 'pwapro_manifest', $manifest );
 	
 	// Delete manifest if it exists.
-	superpwa_delete_manifest();
+	pwapro_delete_manifest();
 	
 	// Write the manfiest to disk.
-	if ( ! superpwa_put_contents( superpwa_manifest( 'abs' ), json_encode( $manifest ) ) ) {
+	if ( ! pwapro_put_contents( pwapro_manifest( 'abs' ), json_encode( $manifest ) ) ) {
 		return false;
 	}
 	
@@ -107,29 +107,29 @@ function superpwa_generate_manifest() {
  * Add manifest to header (wp_head)
  *
  * @since 1.0
- * @since 1.8 Introduced filter superpwa_wp_head_tags
- * @since 1.9 Introduced filter superpwa_add_theme_color
+ * @since 1.8 Introduced filter pwapro_wp_head_tags
+ * @since 1.9 Introduced filter pwapro_add_theme_color
  */
-function superpwa_add_manifest_to_wp_head() {
+function pwapro_add_manifest_to_wp_head() {
 	
 	$tags  = '<!-- Manifest added by SuperPWA - Progressive Web Apps Plugin For WordPress -->' . PHP_EOL; 
-	$tags .= '<link rel="manifest" href="'. parse_url( superpwa_manifest( 'src' ), PHP_URL_PATH ) . '">' . PHP_EOL;
+	$tags .= '<link rel="manifest" href="'. parse_url( pwapro_manifest( 'src' ), PHP_URL_PATH ) . '">' . PHP_EOL;
 	
 	// theme-color meta tag 
-	if ( apply_filters( 'superpwa_add_theme_color', true ) ) {
+	if ( apply_filters( 'pwapro_add_theme_color', true ) ) {
 		
 		// Get Settings
-		$settings = superpwa_get_settings();
+		$settings = pwapro_get_settings();
 		$tags .= '<meta name="theme-color" content="'. $settings['theme_color'] .'">' . PHP_EOL;
 	}
 	
-	$tags  = apply_filters( 'superpwa_wp_head_tags', $tags );
+	$tags  = apply_filters( 'pwapro_wp_head_tags', $tags );
 	
 	$tags .= '<!-- / SuperPWA.com -->' . PHP_EOL; 
 	
 	echo $tags;
 }
-add_action( 'wp_head', 'superpwa_add_manifest_to_wp_head', 0 );
+add_action( 'wp_head', 'pwapro_add_manifest_to_wp_head', 0 );
 
 /**
  * Delete manifest
@@ -138,8 +138,8 @@ add_action( 'wp_head', 'superpwa_add_manifest_to_wp_head', 0 );
  * 
  * @since 1.0
  */
-function superpwa_delete_manifest() {
-	return superpwa_delete( superpwa_manifest( 'abs' ) );
+function pwapro_delete_manifest() {
+	return pwapro_delete( pwapro_manifest( 'abs' ) );
 }
 
 /**
@@ -148,10 +148,10 @@ function superpwa_delete_manifest() {
  * @return	array	An array of icons to be used as the application icons and splash screen icons
  * @since	1.3
  */
-function superpwa_get_pwa_icons() {
+function pwapro_get_pwa_icons() {
 	
 	// Get settings
-	$settings = superpwa_get_settings();
+	$settings = pwapro_get_settings();
 	
 	// Application icon
 	$icons_array[] = array(
@@ -179,7 +179,7 @@ function superpwa_get_pwa_icons() {
  * @return	string	Relative path to the folder where WordPress is installed. Same folder as manifest and wp-config.php
  * @since	1.4
  */
-function superpwa_get_scope() {
+function pwapro_get_scope() {
 	return parse_url( trailingslashit( get_bloginfo( 'wpurl' ) ), PHP_URL_PATH );
 }
 
@@ -189,10 +189,10 @@ function superpwa_get_scope() {
  * @return	string	Orientation of PWA as set in the plugin settings. 
  * @since	1.4
  */
-function superpwa_get_orientation() {
+function pwapro_get_orientation() {
 	
 	// Get Settings
-	$settings = superpwa_get_settings();
+	$settings = pwapro_get_settings();
 	
 	$orientation = isset( $settings['orientation'] ) ? $settings['orientation'] : 0;
 	

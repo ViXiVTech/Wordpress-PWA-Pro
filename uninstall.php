59b430d873450ef0e13a8cd1556b2a05cc6af5ab
ViXiV Technologies <ViXiVTech@gmail.com>
2018-11-04 19:55:44 -0500

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -17,40 +17,40 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) die;
  * Delete database settings
  *
  * @since 1.0
- * @since 1.7 Added clean-up for superpwa_active_addons and superpwa_utm_tracking_settings
+ * @since 1.7 Added clean-up for pwapro_active_addons and pwapro_utm_tracking_settings
  */ 
-delete_option( 'superpwa_settings' );
-delete_option( 'superpwa_active_addons' );
-delete_option( 'superpwa_utm_tracking_settings' );
-delete_option( 'superpwa_version' );
+delete_option( 'pwapro_settings' );
+delete_option( 'pwapro_active_addons' );
+delete_option( 'pwapro_utm_tracking_settings' );
+delete_option( 'pwapro_version' );
 
 /**
  * Clean up for Multisites
  *
  * @since 1.6
- * @since 1.7 Added clean-up for superpwa_active_addons and superpwa_utm_tracking_settings
+ * @since 1.7 Added clean-up for pwapro_active_addons and pwapro_utm_tracking_settings
  */
 if ( is_multisite() ) {
 	
-	// Retrieve the list of blog ids where SuperPWA is active. (saved with blog_id as $key and activation_status as $value)
-	$superpwa_sites = get_site_option( 'superpwa_active_sites' );
+	// Retrieve the list of blog ids where PWA Pro is active. (saved with blog_id as $key and activation_status as $value)
+	$pwapro_sites = get_site_option( 'pwapro_active_sites' );
 	
 	// Loop through each active site.
-	foreach( $superpwa_sites as $blog_id => $actviation_status ) {
+	foreach( $pwapro_sites as $blog_id => $actviation_status ) {
 		
 		// Switch to each blog
 		switch_to_blog( $blog_id );
 		
 		// Delete database settings for each site.
-		delete_option( 'superpwa_settings' );
-		delete_option( 'superpwa_active_addons' );
-		delete_option( 'superpwa_utm_tracking_settings' );
-		delete_option( 'superpwa_version' );
+		delete_option( 'pwapro_settings' );
+		delete_option( 'pwapro_active_addons' );
+		delete_option( 'pwapro_utm_tracking_settings' );
+		delete_option( 'pwapro_version' );
 		
 		// Return to main site
 		restore_current_blog();
 	}
 	
-	// Delete the list of websites where SuperPWA was activated.
-	delete_site_option( 'superpwa_active_sites' );
+	// Delete the list of websites where PWA Pro was activated.
+	delete_site_option( 'pwapro_active_sites' );
 }

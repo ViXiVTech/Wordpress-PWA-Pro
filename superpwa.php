59b8b95f878d57bfd1e502bba56930603766a9f9
ViXiV Technologies <ViXiVTech@gmail.com>
2018-11-04 19:46:28 -0500

    Update superpwa.php

--- superpwa.php
+++ superpwa.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * Plugin Name: Super Progressive Web Apps
- * Plugin URI: https://superpwa.com/?utm_source=superpwa-plugin&utm_medium=plugin-uri
- * Description: Convert your WordPress website into a Progressive Web App
- * Author: SuperPWA
+ * Plugin Name: PWA Pro
+ * Plugin URI: https://www.vixiv.net
+ * Description: PWA Pro helps create Progressive Web Apps out of Responsive Wordpress sites instantly.
+ * Author: SuperPWA, ViXiV Technologies
  * Author URI: https://superpwa.com/?utm_source=superpwa-plugin&utm_medium=author-uri
- * Contributors: Arun Basil Lal, Jose Varghese
- * Version: 1.9
- * Text Domain: super-progressive-web-apps
+ * Author URI: https://www.ViXiV.net
+ * Contributors: Arun Basil Lal, Jose Varghese, ViXiV Technologies
+ * Version: 3.0
+ * Text Domain: pwa-pro
  * Domain Path: /languages
  * License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
@@ -28,7 +29,7 @@
  * index.php					- Dummy file.
  * license.txt					- GPL v2
  * loader.php					- Loads everything.
- * superpwa.php					- Main plugin file.
+ * pwapro.php					- Main plugin file.
  * README.MD					- Readme for GitHub.
  * readme.txt					- Readme for WordPress plugin repository.
  * uninstall.php				- Fired when the plugin is uninstalled. 
@@ -44,9 +45,9 @@ if ( ! defined('ABSPATH') ) exit;
  * @since 1.6 Depreciated constants for multisite compatibility: SUPERPWA_MANIFEST_FILENAME, SUPERPWA_MANIFEST_ABS, SUPERPWA_MANIFEST_SRC
  * @since 1.6 Depreciated constants for multisite compatibility: SUPERPWA_SW_FILENAME, SUPERPWA_SW_ABS, SUPERPWA_SW_SRC
  */
-if ( ! defined( 'SUPERPWA_VERSION' ) )		define( 'SUPERPWA_VERSION'	, '1.9' ); // SuperPWA current version
-if ( ! defined( 'SUPERPWA_PATH_ABS' ) ) 	define( 'SUPERPWA_PATH_ABS'	, plugin_dir_path( __FILE__ ) ); // Absolute path to the plugin directory. eg - /var/www/html/wp-content/plugins/super-progressive-web-apps/
-if ( ! defined( 'SUPERPWA_PATH_SRC' ) ) 	define( 'SUPERPWA_PATH_SRC'	, plugin_dir_url( __FILE__ ) ); // Link to the plugin folder. eg - https://example.com/wp-content/plugins/super-progressive-web-apps/
+if ( ! defined( 'PWAPRO_VERSION' ) )		define( 'PWA_VERSION'	, '3.0' ); // SuperPWA current version
+if ( ! defined( 'PWAPRO_PATH_ABS' ) ) 	define( 'PWA_PATH_ABS'	, plugin_dir_path( __FILE__ ) ); // Absolute path to the plugin directory. eg - /var/www/html/wp-content/plugins/super-progressive-web-apps/
+if ( ! defined( 'PWAPRO_PATH_SRC' ) ) 	define( 'PWA_PATH_SRC'	, plugin_dir_url( __FILE__ ) ); // Link to the plugin folder. eg - https://example.com/wp-content/plugins/super-progressive-web-apps/
 
 // Load everything
 require_once( SUPERPWA_PATH_ABS . 'loader.php' );

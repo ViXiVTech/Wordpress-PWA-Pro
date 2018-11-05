ef0a91c24471ee980d1291ff65119134eea8e05b
ViXiV Technologies <ViXiVTech@gmail.com>
2018-11-04 20:30:39 -0500

    Update multisite.php

--- functions/multisite.php
+++ functions/multisite.php
@@ -4,9 +4,9 @@
  *
  * @since 1.6
  * 
- * @function	superpwa_multisite_filename_postfix()		Filename postfix for multisites
- * @function	superpwa_multisite_activation_status()		Save activation status for current blog id
- * @function	superpwa_multisite_network_deactivator()	Handle multisite network deactivation
+ * @function	pwapro_multisite_filename_postfix()		Filename postfix for multisites
+ * @function	pwapro_multisite_activation_status()		Save activation status for current blog id
+ * @function	pwapro_multisite_network_deactivator()	Handle multisite network deactivation
  */
 
 // Exit if accessed directly
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * 
  * @since 1.6
  */
-function superpwa_multisite_filename_postfix() {
+function pwapro_multisite_filename_postfix() {
 	
 	// Return empty string if not a multisite
 	if ( ! is_multisite() ) {
@@ -40,7 +40,7 @@ function superpwa_multisite_filename_postfix() {
  * 
  * @since 1.6
  */
-function superpwa_multisite_activation_status( $status ) {
+function pwapro_multisite_activation_status( $status ) {
 	
 	// Only for multisites
 	if ( ! is_multisite() || ! isset( $status ) ) {
@@ -48,13 +48,13 @@ function superpwa_multisite_activation_status( $status ) {
 	}
 	
 	// Get current list of sites where SuperPWA is activated.
-	$superpwa_sites = get_site_option( 'superpwa_active_sites', array() );
+	$pwapro_sites = get_site_option( 'pwapro_active_sites', array() );
 	
 	// Set the status for the current blog.
-	$superpwa_sites[ get_current_blog_id() ] = $status;
+	$pwapro_sites[ get_current_blog_id() ] = $status;
 	
 	// Save it back to the database.
-	update_site_option( 'superpwa_active_sites', $superpwa_sites );
+	update_site_option( 'pwapro_active_sites', $pwapro_sites );
 }
 
 /**
@@ -67,7 +67,7 @@ function superpwa_multisite_activation_status( $status ) {
  * This also this gives the user an option to decide if SuperPWA should handle this by changing the defenition of wp_is_large_network.
  * @link https://developer.wordpress.org/reference/functions/wp_is_large_network/
  */
-function superpwa_multisite_network_deactivator() {
+function pwapro_multisite_network_deactivator() {
 	
 	// Do not run on large networks
 	if ( wp_is_large_network() ) {
@@ -75,30 +75,30 @@ function superpwa_multisite_network_deactivator() {
 	}
 	
 	// Retrieve the list of blog ids where SuperPWA is active. (saved with blog_id as $key and activation_status as $value)
-	$superpwa_sites = get_site_option( 'superpwa_active_sites' );
+	$pwapro_sites = get_site_option( 'pwapro_active_sites' );
 	
 	// Loop through each active site.
-	foreach( $superpwa_sites as $blog_id => $actviation_status ) {
+	foreach( $pwapro_sites as $blog_id => $actviation_status ) {
 		
 		// Switch to each blog
 		switch_to_blog( $blog_id );
 		
 		// Delete manifest
-		superpwa_delete_manifest();
+		pwapro_delete_manifest();
 	
 		// Delete service worker
-		superpwa_delete_sw();
+		pwapro_delete_sw();
 		
 		/**
 		 * Delete SuperPWA version info for current blog.
 		 * 
-		 * This is required so that superpwa_upgrader() will run and create the manifest and service worker on next activation.
+		 * This is required so that pwapro_upgrader() will run and create the manifest and service worker on next activation.
 		 * Known edge case: Database upgrade that relies on the version number will fail if user deactivates and later activates after SuperPWA is updated.
 		 */
-		delete_option( 'superpwa_version' );
+		delete_option( 'pwapro_version' );
 	
 		// Save the de-activation status of current blog.
-		superpwa_multisite_activation_status( false );
+		pwapro_multisite_activation_status( false );
 		
 		// Return to main site
 		restore_current_blog();
